fafa464abc6388a76d2ea45180f43191f3df20be
FelipeMadureira <fmadure@gmail.com>
2020-08-26 15:14:33 -0300

    spring boot ex2 15:14

---
+++ Aula08/springsample2/src/main/java/br/madureira/springsample2/dao/DepartamentoDAO.java
@@ -0,0 +1,9 @@
+package br.madureira.springsample2.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.madureira.springsample2.model.Departamento;
+
+public interface DepartamentoDAO extends CrudRepository<Departamento, Integer> {
+    
+}
